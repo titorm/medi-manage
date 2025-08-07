c4a750a9d832fda3ea89f58d3de53f3e4308be90
Tito Ribeiro Motter <titoribeiromotter@gmail.com>
2025-08-07 00:48:31 +0000

    error 404

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,16 +1,14 @@
 import createMiddleware from 'next-intl/middleware';
-
+ 
 export default createMiddleware({
   // A list of all locales that are supported
   locales: ['en', 'es', 'pt'],
-
+ 
   // Used when no locale matches
-  defaultLocale: 'en',
+  defaultLocale: 'en'
 });
-
+ 
 export const config = {
-  // Match all pathnames except for
-  // - … if they start with `/api`, `/_next` or `.` (e.g. `favicon.ico`)
-  // - … the ones containing a dot (e.g. `favicon.ico`)
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+  // Match only internationalized pathnames
+  matcher: ['/', '/(en|es|pt)/:path*']
 };
