146fe498f4de69229ea3c67e2971491f91f771c5
Tito Ribeiro Motter <titoribeiromotter@gmail.com>
2025-08-07 00:15:06 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,8 +1,10 @@
 import {useTranslations} from 'next-intl';
+import {unstable_setRequestLocale} from 'next-intl/server';
 import Link from 'next/link';
  
-export default function Index() {
-  const t = useTranslations('Index');
+export default function Index({params: {locale}}: {params: {locale: string}}) {
+  unstable_setRequestLocale(locale);
+  const t = useTranslations('HomePage');
   return (
     <div>
       <h1>{t('title')}</h1>
